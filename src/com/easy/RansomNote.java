881c43fed3c2d5e70043e319592176e10a2f56c1
Sunny Shah <sunnyshah452@gmail.com>
2018-06-21 23:36:50 +0530

    adding RansomNote Solution

---
+++ src/com/easy/RansomNote.java
@@ -0,0 +1,58 @@
+/**
+ *		
+ */
+package com.easy;
+
+/**
+ *
+Author: 
+	Sunny Shah 
+	sunnyshah452@gmail.com 
+	https://github.com/sunnyshah28
+	https://leetcode.com/sunnyshah28
+	https://www.linkedin.com/in/sunnyshah28
+ *
+ *
+-------------------------------------------------------
+https://leetcode.com/problems/ransom-note/description/
+-------------------------------------------------------
+
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+
+*
+*/
+public class RansomNote {
+	public boolean canConstruct(String ransomNote, String magazine) {
+        int countAlphaRansomNote[] = new int[26];
+        int countAlphaMagazine[] = new int[26];
+        char charsInRansomNote[] = ransomNote.toCharArray();
+        char charsInMagazine[] = magazine.toCharArray();
+        
+        for( char ch: charsInRansomNote ){
+            countAlphaRansomNote[ch-'a']++;
+        }
+        for( char ch: charsInMagazine ){
+            countAlphaMagazine[ch-'a']++;
+        }
+        boolean result = true;
+        
+        for( int i=0 ; i<26 ; i++ ){
+            if( countAlphaMagazine[i] < countAlphaRansomNote[i] ){
+                result = false;
+                break;
+            }
+        }
+        return result;
+        
+    }
+}
