eaa553c57901f179e8e6dcfc03bc9f98aeb64d99
Sunny Shah <sunnyshah452@gmail.com>
2018-07-29 23:09:19 +0530

    SingleNumber Solution

---
+++ src/com/easy/SingleNumber.java
@@ -0,0 +1,46 @@
+/**
+ *		
+ */
+package com.easy;
+
+/**
+ *
+Author: 
+	Sunny Shah 
+	sunnyshah452@gmail.com 
+	https://github.com/sunnyshah28
+	https://leetcode.com/sunnyshah28
+	https://www.linkedin.com/in/sunnyshah28
+ *
+ *
+-------------------------------------------------------
+https://leetcode.com/problems/single-number/description/
+-------------------------------------------------------
+
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+
+*
+*/
+public class SingleNumber {
+	public int singleNumber(int[] nums) {
+        int res = 0;
+        for( int num: nums ){
+            res ^= num;
+        }
+        return res;
+    }
+}
