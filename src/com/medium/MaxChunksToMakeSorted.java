410370af4bb41676c70ceba017236d871b70e8d2
Sunny Shah <sunnyshah452@gmail.com>
2018-09-03 17:32:00 -0400

    MaxChunksToMakeSorted Solution

---
+++ src/com/medium/MaxChunksToMakeSorted.java
@@ -0,0 +1,70 @@
+/**
+ *		
+ */
+package com.medium;
+
+/**
+ *
+Author: 
+	Sunny Shah 
+	sunnyshah452@gmail.com 
+	https://github.com/sunnyshah28
+	https://leetcode.com/sunnyshah28
+	https://www.linkedin.com/in/sunnyshah28
+ *
+ *
+-------------------------------------------------------
+https://leetcode.com/problems/max-chunks-to-make-sorted/description/
+-------------------------------------------------------
+
+Given an array arr that is a permutation of [0, 1, ..., arr.length - 1], we split the array into some number of "chunks" (partitions), and individually sort each chunk.  After concatenating them, the result equals the sorted array.
+
+What is the most number of chunks we could have made?
+
+Example 1:
+
+Input: arr = [4,3,2,1,0]
+Output: 1
+Explanation:
+Splitting into two or more chunks will not return the required result.
+For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], which isn't sorted.
+
+Example 2:
+
+Input: arr = [1,0,2,3,4]
+Output: 4
+Explanation:
+We can split into two chunks, such as [1, 0], [2, 3, 4].
+However, splitting into [1, 0], [2], [3], [4] is the highest number of chunks possible.
+
+Note:
+
+    arr will have length in range [1, 10].
+    arr[i] will be a permutation of [0, 1, ..., arr.length - 1].
+
+*
+*/
+public class MaxChunksToMakeSorted {
+    public int maxChunksToSorted(int[] arr) {
+        
+        int result = 0 ;
+        
+        for( int i=0 ; i<arr.length ; i++ ){
+            
+            if( i == arr[i] ){
+                result++;
+            }
+            else{
+                int maxPos = arr[i];
+                while( i<arr.length && i<=maxPos ){
+                    maxPos = Math.max(maxPos,arr[i]);
+                    i++;
+                }
+                result++;
+                i--;
+            }
+            
+        }
+        return result;
+    }
+}
