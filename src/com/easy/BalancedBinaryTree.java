17376c229f918ed4b4e69a83462fe9007a1ed0b6
Sunny Shah <sunnyshah452@gmail.com>
2018-09-06 13:22:28 -0400

    updating the class

--- src/com/easy/BalancedBinaryTree.java
+++ src/com/easy/BalancedBinaryTree.java
@@ -57,10 +57,10 @@ public class BalancedBinaryTree {
 	 * Definition for a binary tree node.
 	 */
 	public class TreeNode {
-	      int val;
-	      TreeNode left;
-	     TreeNode right;
-	      TreeNode(int x) { val = x; }
+	      public int val;
+	      public TreeNode left;
+	      public TreeNode right;
+	      public TreeNode(int x) { val = x; }
 	  }
 	 
 	
