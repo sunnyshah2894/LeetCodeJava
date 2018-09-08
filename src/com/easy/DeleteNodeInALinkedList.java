f872d66ae209011ea6d651e4b9b6537fa518b845
Sunny Shah <sunnyshah452@gmail.com>
2018-09-08 18:28:48 -0400

    DeleteNodeInaLinkedList Solution

---
+++ src/com/easy/DeleteNodeInALinkedList.java
@@ -0,0 +1,63 @@
+/**
+ *		
+ */
+package com.easy;
+
+/**
+ *
+Author: 
+	Sunny Shah 
+	sunnyshah452@gmail.com 
+	https://github.com/sunnyshah28
+	https://leetcode.com/sunnyshah28
+	https://www.linkedin.com/in/sunnyshah28
+ *
+ *
+-------------------------------------------------------
+https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+-------------------------------------------------------
+
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Given linked list -- head = [4,5,1,9], which looks like following:
+
+    4 -> 5 -> 1 -> 9
+
+Example 1:
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list
+             should become 4 -> 1 -> 9 after calling your function.
+
+Example 2:
+
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list
+             should become 4 -> 5 -> 9 after calling your function.
+
+Note:
+
+    The linked list will have at least two elements.
+    All of the nodes' values will be unique.
+    The given node will not be the tail and it will always be a valid node of the linked list.
+    Do not return anything from your function.
+
+
+
+*
+*/
+public class DeleteNodeInALinkedList {
+	public void deleteNode(ListNode node) {
+        ListNode prevNode = node;
+        ListNode nextNode = node.next;
+        while( nextNode.next!=null ){
+            prevNode.val = nextNode.val;
+            prevNode = nextNode;
+            nextNode = nextNode.next;
+        }
+        prevNode.val = nextNode.val;
+        prevNode.next = null;
+    }
+}
