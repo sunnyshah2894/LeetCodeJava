b2006dd2e2daaf62d43597fb5f797951fefbbcf0
Sunny Shah <sunnyshah452@gmail.com>
2018-09-11 01:15:59 -0400

    InvertBinaryTree solution

---
+++ src/com/easy/InvertBinaryTree.java
@@ -0,0 +1,59 @@
+/**
+ *		
+ */
+package com.easy;
+
+import com.easy.BalancedBinaryTree.TreeNode;
+
+/**
+ *
+Author: 
+	Sunny Shah 
+	sunnyshah452@gmail.com 
+	https://github.com/sunnyshah28
+	https://leetcode.com/sunnyshah28
+	https://www.linkedin.com/in/sunnyshah28
+ *
+ *
+-------------------------------------------------------
+https://leetcode.com/problems/invert-binary-tree/description/
+-------------------------------------------------------
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+*
+*/
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if( root == null )return root;
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        invertTree( root.left );
+        invertTree( root.right );
+        
+        return root;
+        
+    }
+}
