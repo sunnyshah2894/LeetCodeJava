2bacad9f71d3319de16a159980c4e6426793c230
Sunny Shah <sunnyshah452@gmail.com>
2018-11-28 21:39:01 -0500

    CourseSchedule solution

---
+++ src/com/medium/CourseSchedule.java
@@ -0,0 +1,97 @@
+/**
+ *		
+ */
+package com.medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ *
+Author: 
+	Sunny Shah 
+	sunnyshah452@gmail.com 
+	https://github.com/sunnyshah2894
+	https://leetcode.com/sunnyshah28
+	https://www.linkedin.com/in/sunnyshah28
+ *
+ *
+-------------------------------------------------------
+https://leetcode.com/problems/course-schedule/description/
+-------------------------------------------------------
+
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+Example 1:
+
+Input: 2, [[1,0]] 
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+
+Input: 2, [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+
+Note:
+
+    The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+    You may assume that there are no duplicate edges in the input prerequisites.
+
+*
+*/
+public class CourseSchedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        List<Integer> result = new ArrayList<>();
+        List<Integer> parent[] = new List[numCourses+1];
+        int countOutDegree[] = new int[numCourses+1];
+        
+        for( int i=0 ; i<numCourses+1 ; i++ ){
+            parent[i] = new ArrayList<Integer>();
+        }
+        
+        for( int i=0 ; i<prerequisites.length ; i++ ){
+            int x = prerequisites[i][0];
+            int y = prerequisites[i][1];
+          
+            parent[x].add(y);
+            
+            countOutDegree[y]++;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for( int i=0 ; i<numCourses ; i++ ){
+            if( countOutDegree[i] == 0 ){
+                q.offer( i );
+            }
+        }
+        
+        while( !q.isEmpty() ){
+            int curr = q.poll();
+            
+            for( int p : parent[curr] ){
+                countOutDegree[p]--;
+                if( countOutDegree[p] == 0 ){
+                    q.offer(p);
+                }
+            }
+            result.add( curr );
+            
+        }
+        
+        if( result.size() != numCourses )
+            return false;
+        return true;
+    }
+}
